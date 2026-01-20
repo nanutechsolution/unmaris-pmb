09f014d83b0933dd2992507b1b9854ed9095ced2
nanu <ranusate19@gmail.com>
2026-01-20 22:56:37 +0800

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,6 @@ class DatabaseSeeder extends Seeder
             'password' => Hash::make('password'),
         ]);
 
-        $this->call([DummyPendaftarLulusSeeder::class]);
+        $this->call([DummyPendaftarLulusSeeder::class, GelombangSeeder::class, DummyCamabaSeeder::class, FacilitySlideSeeder::class, SiteSettingSeeder::class]);
     }
 }
